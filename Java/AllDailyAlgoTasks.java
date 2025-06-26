753b1f714ccf5b468196b205d51b75e8ab8ef69a
rootikigai <ifykel360@gmail.com>
2025-06-26 23:47:31 +0100

    Converted task to function and called in main...works

--- Java/AllDailyAlgoTasks.java
+++ Java/AllDailyAlgoTasks.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 public class AllDailyAlgoTasks{
+  public static int collectAndPrintSum(int[] scores){
+    int sum = 0;
+    for(int i = 0; i < scores.length; i++){
+	    sum += scores[i];
+    }
+    return sum;
+  }
 	public static void main(String[] args){
-	//collect and print sum
+    int[] testScores = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int result = collectAndPrintSum(testScores);
+    System.out.println(result);
+	  /*collect and print sum
     Scanner input = new Scanner(System.in);
 		int[] scores = new int[10];
 		int sum = 0;
@@ -10,6 +20,6 @@ public class AllDailyAlgoTasks{
       scores[i] = input.nextInt();
 			sum += scores[i];
 		}
-    System.out.println("Sum is " + sum);
+    System.out.println("Sum is " + sum);*/
 	}
 }
