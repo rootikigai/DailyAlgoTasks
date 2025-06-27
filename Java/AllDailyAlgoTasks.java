70cf4477c978ef5d9d1ee78f130ef95695b02904
rootikigai <ifykel360@gmail.com>
2025-06-27 23:39:14 +0100

    Function that collects an array of scores and returns the average

--- Java/AllDailyAlgoTasks.java
+++ Java/AllDailyAlgoTasks.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 public class AllDailyAlgoTasks{
   public static int collectAndPrintSum(int[] scores){
-    int sum = 0;
+    int addScores = 0;
     for(int i = 0; i < scores.length; i++){
-	    sum += scores[i];
+	    addScores += scores[i];
     }
-    return sum;
+    return addScores;
   }
+  public static int collectAndPrintAverage(int[] scores){
+    int addScores = 0;
+    int average = 0;
+    for(int i = 0; i < scores.length; i++){
+	    addScores += scores[i];
+      average = (addScores / scores.length);
+    }
+    return average;
+  }
+
 	public static void main(String[] args){
     int[] testScores = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int result = collectAndPrintSum(testScores);
+    int result = collectAndPrintAverage(testScores);
     System.out.println(result);
 	  /*collect and print sum
     Scanner input = new Scanner(System.in);
