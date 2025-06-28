0819bf848ca2c20e504e88d62c3424416a6aa77f
rootikigai <ifykel360@gmail.com>
2025-06-28 11:30:47 +0100

    Funcion that collects scores and returns sum

--- JavaScript/AllDailyAlgoTasks.js
+++ JavaScript/AllDailyAlgoTasks.js
@@ -1,5 +1,17 @@
 const prompt = require("prompt-sync")();
 //Collect and print sum
+function CollectAndPrintSum(scores){
+    let addScores = 0;
+    for(let i = 0; i < scores.length; i++){
+	    addScores += scores[i];
+    }
+    return addScores
+}
+let studentScores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(CollectAndPrintSum(studentScores))
+
+
+/*
 let scores = [];
 let sum = 0;
 for(let i = 1; i <= 10; i++){
@@ -8,3 +20,4 @@ for(let i = 1; i <= 10; i++){
 	sum = sum + scores[i];
 }
 console.log("Sum is " + sum);
+*/
