31b9eb39c21718b6dd021c80ad573c11e50f8c78
rootikigai <ifykel360@gmail.com>
2025-06-28 11:42:36 +0100

    Function that takes numbers and returns sum

--- Python/all_daily_algo_tasks.py
+++ Python/all_daily_algo_tasks.py
@@ -1,9 +1,18 @@
 #Collect and print sum
-scores = []
-addScores = 0
-for i in range(1, 11, 1):
-    score = int(input(f"Enter score {i}: "))
-    scores.append(score)
-    addScores = addScores + score
-print(f"Sum is: {addScores}")
+def collect_and_print_sum(scores):
+    add_scores = 0
+    for score in scores:
+	    add_scores += score;
+    return add_scores
+student_scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(collect_and_print_sum(student_scores))
+
+
+#scores = []
+#addScores = 0
+#for i in range(1, 11, 1):
+#    score = int(input(f"Enter score {i}: "))
+#    scores.append(score)
+#    addScores = addScores + score
+#print(f"Sum is: {addScores}")
 
