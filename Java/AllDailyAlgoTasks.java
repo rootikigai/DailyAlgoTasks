90bf676fd103071ed512702a1ebaab30e188662c
rootikigai <ifykel360@gmail.com>
2025-07-07 06:48:15 +0100

    Refactored the code better

--- Java/AllDailyAlgoTasks.java
+++ Java/AllDailyAlgoTasks.java
@@ -83,7 +83,7 @@ public class AllDailyAlgoTasks{
     //Print a message inside a box made of stars
       for(int i = 1; i <= 10; i++){
           for(int j = 1; j <= 10; j++){
-              if(i == 1 || i == 10) {
+              if(i == 1 || j == 1 || i == 10 || j == 10){
                   System.out.print("* ");
               }
               else if(j == 1 || j == 10) {
