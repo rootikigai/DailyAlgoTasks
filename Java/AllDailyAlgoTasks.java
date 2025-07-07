e3e9f82823f44018fa880f7e2d9503423b32f027
rootikigai <ifykel360@gmail.com>
2025-07-07 04:24:04 +0100

    Back to some basics

--- Java/AllDailyAlgoTasks.java
+++ Java/AllDailyAlgoTasks.java
@@ -33,15 +33,28 @@ public class AllDailyAlgoTasks{
   public static void main(String[] args){
     int result = AllDailyAlgoTasks.divisibleByFive(5);
     System.out.println(result);
+
+    //Print numbers from 1 to 10
+      for(int i = 1; i <= 10; i++){
+          System.out.print(i + " ");
+      }
+      System.out.println();
+    //Print even numbers from 1 to 20
+      for(int i = 1; i <= 20; i++){
+          if(i % 2 == 0){
+              System.out.print(i + " ");
+          }
+      }
+      System.out.println();
+    //Print your name 5 times
+      for(int i = 1; i <= 5; i++) {
+          System.out.print("Kelvin Ifeanyi ");
+      }
+      System.out.println();
+    //Print all numbers from 50 to 100
+      for(int i = 50; i <= 100; i++){
+          System.out.print(i + " ");
+      }
   }
 }
-/*collect and print sum
-    Scanner input = new Scanner(System.in);
-		int[] scores = new int[10];
-		int sum = 0;
-		for(int i = 0; i < 10; i++){
-      System.out.print("Enter score " + (i + 1) + ": ");
-      scores[i] = input.nextInt();
-			sum += scores[i];
-		}
-    System.out.println("Sum is " + sum);*/
+
