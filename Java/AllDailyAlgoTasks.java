2a5310655277e8ce1800e4888219bd5266dd32ba
rootikigai <ifykel360@gmail.com>
2025-07-07 06:45:50 +0100

    Printing message in a hollow box of stars

--- Java/AllDailyAlgoTasks.java
+++ Java/AllDailyAlgoTasks.java
@@ -79,6 +79,22 @@ public class AllDailyAlgoTasks{
               System.out.print(i + " ");
           }
       }
+      System.out.println();
+    //Print a message inside a box made of stars
+      for(int i = 1; i <= 10; i++){
+          for(int j = 1; j <= 10; j++){
+              if(i == 1 || i == 10) {
+                  System.out.print("* ");
+              }
+              else if(j == 1 || j == 10) {
+                  System.out.print("* ");
+              }
+              else{
+                  System.out.print(" "+" ");
+              }
+          }
+          System.out.println();
+      }
   }
 }
 
