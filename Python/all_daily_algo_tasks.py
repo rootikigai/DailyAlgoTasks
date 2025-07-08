8d19e38b23840b684ccabca491b6a45b211284ed
rootikigai <ifykel360@gmail.com>
2025-07-08 21:11:17 +0100

    Basic Algo 1

--- Python/all_daily_algo_tasks.py
+++ Python/all_daily_algo_tasks.py
@@ -2,30 +2,25 @@
 def collect_and_print_sum(scores):
     add_scores = 0
     for score in scores:
-	    add_scores += score;
+        add_scores += score
     return add_scores
 
 def collect_and_print_average(scores):
     add_scores = 0
     average = 0
     for score in scores:
-	    add_scores += score;
+        add_scores += score
     average = (add_scores / len(scores))
     return average
 
-def divisibleByFive(number):
+def divisible_by_five(number):
     if number % 5 == 0:
         return number * number
     return number % 5
 
-print(divisibleByFive(32))
+#print(divisibleByFive(32))
 
-
-#scores = []
-#addScores = 0
-#for i in range(1, 11, 1):
-#    score = int(input(f"Enter score {i}: "))
-#    scores.append(score)
-#    addScores = addScores + score
-#print(f"Sum is: {addScores}")
+#Print numbers from 1 to 10
+for num in range (1, 11, 1):
+    print(num, end= " ")
 
