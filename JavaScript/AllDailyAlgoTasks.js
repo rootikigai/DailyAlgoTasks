098328b135e9bf22286f6dde9d61516de5b867f0
rootikigai <ifykel360@gmail.com>
2025-07-08 21:35:41 +0100

    Basic Algo 2

--- JavaScript/AllDailyAlgoTasks.js
+++ JavaScript/AllDailyAlgoTasks.js
@@ -27,10 +27,12 @@ function DivisibleByFive(number){
 
 // console.log(DivisibleByFive(22))
 
-/* Print numbers from 1 to 10
+// Print numbers from 1 to 10
 for(let i = 1; i <= 10; i++){
     console.log(i)
-}*/
+}
+
+console.log()
 
 // Print even numbers from 1 to 20
 for(let i = 1; i <= 10; i++){
