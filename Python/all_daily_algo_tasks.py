a6aebedd868d5138ac7a9468af8a43b5cbe13b1f
rootikigai <ifykel360@gmail.com>
2025-07-09 22:24:17 +0100

    Basic Algo 5

--- Python/all_daily_algo_tasks.py
+++ Python/all_daily_algo_tasks.py
@@ -42,3 +42,12 @@ print()
 #Print all numbers from 50 to 100
 for num in range(50, 101, 1):
     print(num, end= " ")
+
+print()
+
+#Print all odd numbers between 1 and 30
+for num in range (1, 31, 1):
+    if num % 2 != 0:
+        print(num, end= " ")
+
+print()
