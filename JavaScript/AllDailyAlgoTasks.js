6b650949033e33fc2000ec50aac35cb91a145cab
rootikigai <ifykel360@gmail.com>
2025-07-10 17:27:14 +0100

    Basic Algo 9

--- JavaScript/AllDailyAlgoTasks.js
+++ JavaScript/AllDailyAlgoTasks.js
@@ -84,3 +84,20 @@ for(let i = 1; i <= 100; i++)
 
 console.log()
 
+//Print a message inside a box made of stars
+for(let i = 1; i <= 10; i++){
+    for(let j = 1; j <= 10; j++){
+        if(i == 1 || j == 1 || i == 10 || j == 10){
+            process.stdout.write("* ");
+        }
+        else if(i == 5 && j == 5){
+            process.stdout.write("OK");
+        }
+        else{
+            process.stdout.write(" "+" ");
+        }
+    }
+    console.log();
+}
+
+console.log();
