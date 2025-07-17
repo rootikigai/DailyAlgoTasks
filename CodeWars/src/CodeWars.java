7b1ea2a7ce9fcf47b45e839c133f2d03da578a10
rootikigai <ifykel360@gmail.com>
2025-07-17 23:57:55 +0100

    (Updated Function) CodeWars: Sum Of Multiples below a particular Integer - Updated function to throw exception if illegal argument(s) is passed.

--- CodeWars/src/CodeWars.java
+++ CodeWars/src/CodeWars.java
@@ -1,6 +1,9 @@
 public class CodeWars{
     public static long sumMul(int n, int m){
         long sum = 0;
+        if(n <= 0 || m <= 0){
+            throw new IllegalArgumentException();
+        }
         for(long i = 1; i <= m; i++){
             if(i % n == 0){
                 sum += i;
