6e0e21747e5406f1c3079f850b5c3afc1203945f
rootikigai <ifykel360@gmail.com>
2025-07-17 23:56:33 +0100

    CodeWars: Sum Of Multiples below a particular Integer - Wrote another test that throws exception if illegal argument(s) is passed.

--- CodeWars/test/CodeWarsTest.java
+++ CodeWars/test/CodeWarsTest.java
@@ -1,9 +1,20 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CodeWarsTest {
     @Test
     public void testThatArgumentsReturnSumOfMultiples(){
         assertEquals(20, CodeWars.sumMul(2, 9));
+    }
+    @Test
+    public void testThatInvalidArgumentsThrowsException(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            CodeWars.sumMul(0, 0);
+            CodeWars.sumMul(9, 0);
+            CodeWars.sumMul(0, 9);
+            CodeWars.sumMul(-2, 9);
+            CodeWars.sumMul(2, -9);
+        });
     }
 }
