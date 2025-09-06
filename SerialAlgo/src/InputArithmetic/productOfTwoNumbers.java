46fc12110469526ee433454a58a44f5608b8f151
rootikigai <ifykel360@gmail.com>
2025-09-06 23:45:36 +0100

    Daily Algos: Back to basics - product of two numbers.

---
+++ SerialAlgo/src/InputArithmetic/productOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package InputArithmetic;
+
+public class productOfTwoNumbers {
+    public static int potn(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static void main(String[] args){
+        System.out.println(potn(5, 6));
+    }
+}
