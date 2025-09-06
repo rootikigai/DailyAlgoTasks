1158f5fa8f10ed938ad7e6ec49c22cb0b4573136
rootikigai <ifykel360@gmail.com>
2025-09-06 23:40:41 +0100

    Daily Algos: Back to basics - difference of two numbers.

---
+++ SerialAlgo/src/InputArithmetic/differenceOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package InputArithmetic;
+
+public class differenceOfTwoNumbers {
+    public static int dotn(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static void main(String[] args) {
+        System.out.println(dotn(5, 6));
+    }
+}
