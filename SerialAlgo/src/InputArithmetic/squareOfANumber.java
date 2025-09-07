e7c26d5235a04644f382b4222919fae8c089d5af
rootikigai <ifykel360@gmail.com>
2025-09-07 23:30:18 +0100

    Daily Algos: Back to basics - square of a number.

---
+++ SerialAlgo/src/InputArithmetic/squareOfANumber.java
@@ -0,0 +1,10 @@
+package InputArithmetic;
+
+public class squareOfANumber {
+    public static int square(int number){
+        return number*number;
+    }
+    public static void main (String[] args){
+        System.out.println(square(12));
+    }
+}
